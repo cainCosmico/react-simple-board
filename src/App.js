d92df4228c42acf47dd02303cff5b48da2b8355d
Camilo Valencia <cromalcocamilovro@gmail.com>
2020-08-01 01:40:04 -0500

    Sismple dash board iniciado con react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// import { Aforo } from "./aforo/Aforo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
+          <Hello title='Hello from props'/>
+        </p> */}
+
+        {/* <div>
+          <Aforo title='Hola soy aforo'/>
+        </div> */}
+
+        {/* <Text 
+        arrayOfNumbers={[2,45,8]}
+        objectWithInfo={ {key:'value', key2:'otherValue'} }
+        number={2} 
+        text='Texto en string' 
+        boolean={true}
+        isActivate/> */}
+
+        <Aforo 
+        
+        actual={38}
+        disponible={12}
+        estado={true}
+        alphaTiempo='12 min apox'
+
+        />
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Lee la doc prro
+        </a> */}
       </header>
     </div>
   );
 }
+
+// function Hello(props)
+// {
+// return <h1>Bienvenidos a React App, { props.title }</h1>
+// }
+
+class Hello extends Component
+{
+  render () 
+  {
+    return <h1>Bienvenidos a React App, { this.props.title }</h1>
+  }
+}
+
+class Text extends Component {
+  render () {
+    const textoSegundoBool = this.props.isActivate ? 'On' : 'Off';
+    const mappedNumbers = this.props.arrayOfNumbers.map( n=> n*2 );
+
+    return (
+      <div>
+        <p> { this.props.text } </p> 
+        <p> { this.props.number } </p>
+        <p> { JSON.stringify(this.props.boolean) } </p>
+        <p> { this.props.isActivate } </p>
+        <p> { textoSegundoBool } </p>
+        <p> { this.props.arrayOfNumbers.join(', ') } </p>
+        <p> { mappedNumbers.join(', ') } </p>
+        <p> { this.props.objectWithInfo.key } </p>
+      </div>
+    )
+  }
+  
+}
+
+class Aforo extends Component {
+
+  // this.state = {
+
+  // }
+
+
+  render() {
+
+    const {
+      estado,
+      actual,
+      disponible,
+      alphaTiempo,      
+    } = this.props;
+
+    const estadoCli = estado ? 'Siga' : 'Espere'
+
+     return (
+      <div>
+          <h2> { actual } </h2>
+          <h2> { disponible } </h2>
+          <h2> { alphaTiempo } </h2>
+          <h2> { estadoCli } </h2>
+      </div>
+
+     )
+  }
+}
+
 
 export default App;
---
+++ src/aforo/Aforo.js
@@ -0,0 +1,19 @@
+import { Component } from 'react'
+
+
+class Aforo extends Component {
+
+   // this.state = {
+
+   // }
+
+   render() {
+      return 
+      <div>
+         <h2> { this.props.title } </h2>
+      </div>
+   }
+}
+
+
+module.export.Aforo;
