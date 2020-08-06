7395fd32752b2e40a9d85ec24fde84ae5fa4b5f6
Camilo Valencia <cromalcocamilovro@gmail.com>
2020-08-06 10:09:35 -0500

    added docs

---
+++ docs/precache-manifest.334211a8c760dc51edaa706aa6e864c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057cbb84d0f9f9481c885066fa55672b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "484585614f83dbe00c00",
+    "url": "/static/css/main.03fb1f4a.chunk.css"
+  },
+  {
+    "revision": "16b11f239ae2ca567885",
+    "url": "/static/js/2.56b12b41.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.56b12b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484585614f83dbe00c00",
+    "url": "/static/js/main.8a2fcd64.chunk.js"
+  },
+  {
+    "revision": "fe9ffaf54f75f396f241",
+    "url": "/static/js/runtime-main.db60fa84.js"
+  }
+]);
